1963b4d7785e31b0ecbb2fe49e5a8c3574cc14a8
Nina <ninna@tutanota.com>
2020-04-19 01:39:05 +0400

    new background fixes

--- pages/team.js
+++ pages/team.js
@@ -1,10 +1,12 @@
 import Layout from '../components/MainLayout';
 import {Team as TeamComponent} from '../components/Team';
+import Footer from '../components/Footer';
 
 export default function Team() {
   return (
     <Layout>
     	<TeamComponent/>
+    	<Footer/>
     </Layout>
   );
 }
